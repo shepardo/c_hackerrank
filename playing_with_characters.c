649dddb9365e7c12d2c60adcb3be053fa9c8503f
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-08-11 22:39:51 -0500

    First checkpoint for "Playing with Characters" solution.

---
+++ playing_with_characters.c
@@ -0,0 +1,12 @@
+// https://www.hackerrank.com/challenges/playing-with-characters/problem
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
