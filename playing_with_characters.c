10cc305859f2e38792f3f1b3d60d324fc0b2291f
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-08-12 23:05:59 -0500

    Final solution for "playing with characters".

--- playing_with_characters.c
+++ playing_with_characters.c
@@ -4,9 +4,22 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define MAX_LEN 100
+
 int main() 
 {
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    char ch;
+    char buf[MAX_LEN];
+    scanf("%c", &ch);
+    getchar();
+    printf("%c\n", ch);
+    fgets(buf, sizeof(buf), stdin);
+    //getchar();
+    printf("%s", buf);
+    buf[0] = '\0';
+    fgets(buf, sizeof(buf), stdin);
+    printf("%s", buf);
     return 0;
 }
